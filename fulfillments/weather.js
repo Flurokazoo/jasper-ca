f01af5e2ac7ca4ddfffb450b311b44710de58693
Jasper <jasper_thecrown@Hotmail.com>
2019-11-13 15:54:37 +0100

    Add parameter/entity parsing for 'check weather' intent.

--- fulfillments/weather.js
+++ fulfillments/weather.js
@@ -6,7 +6,7 @@
 module.exports = {
 
     fulfillment: function (agent) {
-    
+        const [city, time, location, flightNo, date] = [agent.parameters['geo-city'], agent.parameters['time'], agent.parameters['location'], agent.parameters['flight-number'], agent.parameters['date']]
         agent.add(
             `Check the weather.`
         )
