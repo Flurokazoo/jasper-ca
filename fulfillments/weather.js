4a6d74ce20f951e9e0cadda2cc4f892a831baec2
Jasper <jasper_thecrown@Hotmail.com>
2019-11-13 17:18:40 +0100

    Add basic axios logic

--- fulfillments/weather.js
+++ fulfillments/weather.js
@@ -6,7 +6,14 @@
 module.exports = {
 
     fulfillment: function (agent) {
+        const axios = require('axios')
+
         const [city, time, location, flightNo, date] = [agent.parameters['geo-city'], agent.parameters['time'], agent.parameters['location'], agent.parameters['flight-number'], agent.parameters['date']]
+        axios.get('https://jsonplaceholder.typicode.com/todos/1')
+            .then(function (response) {
+                console.log(response.data);
+            })
+           
         agent.add(
             `Check the weather.`
         )
