e1597a5921af60fc8a2fe680bcc1d6e9fdccd196
Jasper <jasper_thecrown@Hotmail.com>
2019-11-16 21:24:51 +0100

    Add comments to code

--- fulfillments/weather.js
+++ fulfillments/weather.js
@@ -9,32 +9,47 @@ module.exports = {
 
     fulfillment: function (agent) {
 
-        console.log(config.flightApiKey)
-
         const [city, time, flightNo, date] = [agent.parameters['geo-city'], agent.parameters['time'], agent.parameters['flight-number'], agent.parameters['date']]
-        let location = agent.parameters['location']
-        const flightNoFormatted = flightNo.replace(/\s/g, "")
+        let location = agent.parameters['location']             //Location may need to be filled so it's declared a let instead of const
+        const flightNoFormatted = flightNo.replace(/\s/g, "")   //Strips the provided flight number from whitespaces for API use
         let latLng = {}
 
+        /**
+         * Code to be executed when a flight number is provided by the user
+         */
+
         if (flightNo) {
+
+            //If non relevant data for a flight number is provided a response asking to clarify will be returned
             if (city || time || date) {
                 agent.add(`That's a lot of data. Please either ask me the weather for your flight or by location. Thank you :)`)
             } else {
-                return axios.get(`http://aviation-edge.com/v2/public/flights?key=` + config.flightApiKey + `&flightIata=` + flightNoFormatted)
+                //Get data regarding the provided flight number
+                axios.get(`http://aviation-edge.com/v2/public/flights?key=` + config.flightApiKey + `&flightIata=` + flightNoFormatted)
                     .then((response) => {
+
+                        //Declaration of variable for the IATA number of an airport (this acts as a unique identifier)
                         let iataAirport
+
+                        //Determine the asked location of the user: Departure airport, arrival airport or the current location of the flight
                         if (location == `current location`) {
+                            //Set latitude and longitude for current location
                             latLng.latitude = response.data[0].geography.latitude
                             latLng.longitude = response.data[0].geography.longitude
                         } else {
+
+                            //If either departure of arrival is selected an additional API call needs to be made to determine the location of the airport
                             if (location == `departure`) {
                                 iataAirport = response.data[0].departure.iataCode
                             } else {
-                                location = `arrival`
+                                location = `arrival`    //If the location entity isn't provided, it will default to the arrival location
                                 iataAirport = response.data[0].arrival.iataCode
                             }
+
+                            //Get data about either the departure or arrival airport
                             axios.get(`https://aviation-edge.com/v2/public/airportDatabase?key=` + config.flightApiKey + `&codeIataAirport=` + iataAirport)
                                 .then((response) => {
+                                    //Set latitude and longitude for airport
                                     latLng.latitude = response.data[0].latitudeAirport
                                     latLng.longitude = response.data[0].longitudeAirport
                                 })
