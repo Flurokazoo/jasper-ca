22d17049de1b81d02810f0b8e82088ffc7ebe3d7
Jasper <jasper_thecrown@Hotmail.com>
2019-11-17 20:37:09 +0100

    Add logic for calculating time

--- fulfillments/weather.js
+++ fulfillments/weather.js
@@ -5,7 +5,7 @@
 const axios = require('axios')
 const config = require('../config')
 const moment = require('moment')
-
+const weather = require('../weather')
 module.exports = {
 
     fulfillment: function (agent) {
@@ -13,7 +13,6 @@ module.exports = {
         const [city, time, flightNo, date] = [agent.parameters['geo-city'], agent.parameters['time'], agent.parameters['flight-number'], agent.parameters['date']]
         let location = agent.parameters['location']             //Location may need to be filled so it's declared a let instead of const
         const flightNoFormatted = flightNo.replace(/\s/g, "")   //Strips the provided flight number from whitespaces for API use
-        let latLng = {}
 
         /**
          * Code to be executed when a flight number is provided by the user
@@ -34,6 +33,9 @@ module.exports = {
                         //Declaration of variable for the type of weather method (either future prediction current)
                         let weatherMethod
 
+                        let latLng = {}
+
+
                         //Determine the asked location of the user: Departure airport, arrival airport or the current location of the flight
                         if (location == `current location`) {
                             //Set latitude and longitude for current location
@@ -41,6 +43,7 @@ module.exports = {
                             latLng.longitude = response.data[0].geography.longitude
                             weatherMethod = `current`
                         } else {
+
 
                             //If either departure of arrival is selected an additional API call needs to be made to determine the location of the airport
                             if (location == `departure`) {
@@ -54,31 +57,65 @@ module.exports = {
                             }
 
                             //Get data about either the departure or arrival airport
-                            axios.get(`https://aviation-edge.com/v2/public/airportDatabase?key=` + config.flightApiKey + `&codeIataAirport=` + iataAirport)
+                            return axios.get(`https://aviation-edge.com/v2/public/airportDatabase?key=` + config.flightApiKey + `&codeIataAirport=` + iataAirport)
                                 .then((response) => {
+                                    let offset = parseInt(response.data[0].GMT)
+
                                     //Set latitude and longitude for airport
                                     latLng.latitude = response.data[0].latitudeAirport
                                     latLng.longitude = response.data[0].longitudeAirport
-                                    console.log(response.data)
                                     if (location == `arrival`) {
-                                        axios.get(`http://aviation-edge.com/v2/public/timetable?key=` + config.flightApiKey + `&iataCode=` + iataAirport + `&type=arrival&flight_iata=` + flightNoFormatted)
+                                        return axios.get(`http://aviation-edge.com/v2/public/timetable?key=` + config.flightApiKey + `&iataCode=` + iataAirport + `&type=arrival&flight_iata=` + flightNoFormatted)
                                             .then((response) => {
+                                                let time
+                                                time = moment(response.data[0].arrival.scheduledTime).unix()
+                                                if(offset > 0){
+                                                    offset = -Math.abs(offset * 3600)
+                                                } else if(offset < 0) {
+                                                    offset = Math.abs(offset * 3600)
+                                                }
+                                     
+                                                return weather.get({
+                                                    lat: latLng.latitude,
+                                                    lon: latLng.longitude,
+                                                    key: config.weatherApiKey,
+                                                    method: `forecast/hourly`
+                                                })
+                                                    .then((response) => {
+                                                        let weatherItems = {}
+                                                        agent.add(
+                                                            `http://aviation-edge.com/v2/public/timetable?key=` + config.flightApiKey + `&iataCode=` + iataAirport + `&type=arrival&flight_iata=` + flightNoFormatted
+                                                        )
+
+                                                        for (let entry of response) {
+                                                            //console.log(entry.ts)
+                                                          }
+                                                    }).catch(error => {
+                                                        console.log(error)
+                                                    })
                                                 //flightData = response.data.title
-                                                console.log(moment(response.data[0].arrival.estimatedTime).unix())
-                                                console.log(moment())
+                                                // console.log(moment(response.data[0].arrival.estimatedTime).unix())
+                                                // console.log(moment())
+                                            }).catch(error => {
+                                                console.log(error)
                                             })
                                     }
+                                }).catch(error => {
+                                    console.log(error)
                                 })
                         }
 
-                        return axios.get(`http://aviation-edge.com/v2/public/flights?key=` + config.flightApiKey + `&flightIata=` + flightNoFormatted)
-                            .then((response) => {
-                                //flightData = response.data.title
-                                agent.add(
-                                    `Latitude: ` + latLng.latitude + `, Longitude: ` + latLng.longitude
-                                )
-                            })
+                        // return axios.get(`http://api.weatherbit.io/v2.0/current?lat=` + latLng.latitude + `&lon=` + latLng.longitude)
+                        //     .then((response) => {
+                        //         //flightData = response.data.title
+                        //         console.log(response.data)
+                        //         agent.add(
+                        //             `Latitude: ` + latLng.latitude + `, Longitude: ` + latLng.longitude
+                        //         )
+                        //     })
 
+                    }).catch(error => {
+                        console.log(error)
                     })
             }
         }
