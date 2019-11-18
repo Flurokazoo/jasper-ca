eed096ec120e7f0e7c0652ac0e67a4631e78e35a
Jasper <jasper_thecrown@Hotmail.com>
2019-11-18 20:17:54 +0100

    Add response for altitude intent.

--- fulfillments/altitude.js
+++ fulfillments/altitude.js
@@ -8,8 +8,35 @@ const config = require('../config')
 module.exports = {
 
     fulfillment: function (agent) {
+        const flightNo = agent.parameters['flight-number']
+        const flightNoFormatted = flightNo.replace(/\s/g, "")
 
-        
+        if (flightNo) {
+
+            return axios.get(`http://aviation-edge.com/v2/public/flights?key=` + config.flightApiKey + `&flightIata=` + flightNoFormatted)
+                .then((response) => {
+                    let altitude
+                    if (response.data.error) {
+                        agent.add(
+                            `I'm sorry, but it looks like that flight is not registered. Please ask my again with a valid flight number or city`
+                        )
+                    } else {
+                        altitude = response.data[0].geography.altitude
+                        agent.add(
+                            `Our current altitude is ` + altitude + ` feet. Happy flight!`
+                        )
+                    }
+
+                }).catch(error => {
+                    console.log(error)
+                })
+
+        } else {
+            agent.add(
+                `I'm sorry, but I need to know our flight number to fulfill this request.`
+            )
+        }
+
     }
 
 }
