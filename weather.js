2004006725105756a89879e45e0f8e063654bf96
Jasper <jasper_thecrown@Hotmail.com>
2019-11-18 17:07:24 +0100

    Build dynamic URL setting for searching by latlon or city.

--- weather.js
+++ weather.js
@@ -3,13 +3,19 @@ const axios = require('axios')
 
 module.exports = {
     get: function (data) {
-        return axios.get(`http://api.weatherbit.io/v2.0/` + data.method + `?lat=` + data.lat + `&lon=` + data.lon + `&key=` + data.key)
+        let url
+        if(data.city){
+            url = `http://api.weatherbit.io/v2.0/` + data.method + `?city=` + data.city + `&key=` + data.key
+            console.log(url)
+        } else if (data.lat && data.lon){
+            url = `http://api.weatherbit.io/v2.0/` + data.method + `?lat=` + data.lat + `&lon=` + data.lon + `&key=` + data.key
+        }
+        return axios.get(url)
             .then((response) => {
                 return(response.data.data)
-                //return(`http://api.weatherbit.io/v2.0/` + data.method + `?lat=` + data.lat + `&lon=` + data.lon + `&key=` + data.key)
                 
             }).catch(error =>{
-                console.log(error)
+                //console.log(error)
             })
     }    
 }
