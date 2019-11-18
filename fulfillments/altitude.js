4ddf7a01ce010d22e1f9277c3cf7951064115ca1
Jasper <jasper_thecrown@Hotmail.com>
2019-11-18 20:02:20 +0100

    Add altitude intent to project

---
+++ fulfillments/altitude.js
@@ -0,0 +1,15 @@
+/**
+ * Intent: Check Altitude Intent
+ * Fulfillment: default
+ */
+const axios = require('axios')
+const config = require('../config')
+
+module.exports = {
+
+    fulfillment: function (agent) {
+
+        
+    }
+
+}
--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const app = express()
 const f_welcome = require('./fulfillments/default/welcome')
 const f_fallback = require('./fulfillments/default/fallback')
 const f_weather = require('./fulfillments/weather')
+const f_altitude = require('./fulfillments/altitude')
+
 
 // Todo: create and require a fulfillment module for each custom intent
 // Hint: use the directory fulfillments/custom
@@ -24,6 +26,7 @@ app.post('/', express.json(), (req, res) => {
   intentMap.set('Default Welcome Intent', f_welcome.fulfillment)
   intentMap.set('Default Fallback Intent', f_fallback.fulfillment)
   intentMap.set('Check weather', f_weather.fulfillment)
+  intentMap.set('Check altitude', f_altitude.fulfillment)
 
   // Todo: connect each custom intent with custom fulfillment modules
   // Hint: create a intent in Dialogflow first
